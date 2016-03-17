d4805b86b3c0d8a0a4a50b458db4e9a916caa7b5
Ömür Buruk <burukomur@gmail.com>
2016-03-17 14:06:22 +0200

    profil demo yapi

---
+++ profil_yapi.php
@@ -0,0 +1,51 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Profil</title>
+</head>
+
+<body>
+<form action="" method="get" name="banner" target="_top">
+
+<table width="100%" border="0">
+  <tr>
+    <th width="18%" rowspan="4" scope="row"><img id="profilfoto" src="" width="100px" height="100px" /></th>
+    <td width="65%" align="center"><p>Ömür Buruk </p></td>
+    <td width="17%" rowspan="3"> <center></center></td>
+  </tr>
+  <tr>
+    <td>Site Puanı : 75</td>
+    </tr>
+  <tr>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td><div align="center">Çıkış</div></td>
+  </tr>
+</table>
+
+</form>
+<form  action="" method="get" name="banner" target="_top">
+<div style="width:100%; height:80%">
+
+<div id="solmenu" style="width:30%; min-height:300px; float:left;">
+<ul >
+<li> Profilim </li>
+<li> Mesajlar </li>
+<li> Ayarlar </li>
+<li> Cv Düzenle </li>
+<li> Şirketler </li>
+<li> Bildirimlerim </li>
+</ul>
+</div>
+<div id="sagicerik" style="width:70%; min-height:300px; float:right;">
+</div>
+</div>
+
+</form>
+
+
+</body>
+</html>
