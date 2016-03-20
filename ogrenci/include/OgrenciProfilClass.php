8dba860df679f1c2ff57940d075f5e64f453adae
burak95 <burakbuyurukcu214@gmail.com>
2016-03-20 18:36:43 +0200

    proje yapısı ve ogrenci profil ekleme

--- ogrenci/include/OgrenciProfilClass.php
+++ ogrenci/include/OgrenciProfilClass.php
@@ -4,25 +4,36 @@ class Ogrenci
 	private $id; //sonra bakılacak
 	private $ad;
 	private $soyad;
+	private $bolum;
+	private $fakulte;
+	private $universite;
+	private $sinif;
 	private $okulNu;
 	private $fotoraf;
 	private $telNu;
 	private $cinsiyet;
-	private $adres;
+	private $kalanAdres;
 	private $tcKimNo;
 	private $dogumTarihi;
 	private $mail;
+	private $il;
+	private $ilce;
 	
-	function Ogrenci ($tcKimNo,$ad,$soyad,$dogumTarihi,$cinsiyet,$adres,$okulNu,$bolum,$telNu,$mail,$fotoraf)
+	function Ogrenci ($tcKimNo,$ad,$soyad,$dogumTarihi,$cinsiyet,$kalanAdres,$okulNu,$bolum,$telNu,$mail,$fotoraf,$fakulte,$universite,$sinif,$il,$ilce)
 	{
 		$this -> ad=$ad;
 		$this -> soyad=$soyad;
 		$this -> okulNu=$okulNu;
 		$this -> bolum=$bolum;
 		$this -> fotoraf=$fotoraf;
+		$this -> il=$il;
+		$this -> ilce=$ilce;
 		$this -> telNu=$telNu;
 		$this -> dogumTarihi=$dogumTarihi;
-		$this -> adres=$adres;
+		$this -> kalanAdres=$kalanAdres;
+		$this -> sinif=$sinif;
+		$this -> fakulte=$fakulte;
+		$this -> universite=$universite;
 		$this -> mail=$mail;
 		$this -> tcKimNo=$tcKimNo;
 		$this -> cinsiyet=$cinsiyet;
@@ -79,13 +90,13 @@ class Ogrenci
 	{
         $this->cinsiyet;
     }
-	function setAdres($adres)
+	function setKalanAdres($kalanAdres)
 	{  
-		$this -> adres = $adres; 
+		$this -> kalanAdres = $kalanAdres; 
 	}
-	function getAdres() 
+	function getKalanAdres() 
 	{
-        $this->adres;
+        $this->kalanAdres;
     }
 	function setTcKimNo($tcKimNo)
 	{  
@@ -110,6 +121,46 @@ class Ogrenci
 	function getMail() 
 	{
         $this->mail;
+    }
+	function setFakulte($fakulte)
+	{  
+		$this -> fakulte = $fakulte; 
+	}
+	function getFakulte() 
+	{
+        $this->fakulte;
+    }	
+	function setUniversite($universite)
+	{  
+		$this -> universite = $universite; 
+	}
+	function getUniversite() 
+	{
+        $this->universite;
+    }
+	function setSinif($sinif)
+	{  
+		$this -> sinif = $sinif; 
+	}
+	function getSinif() 
+	{
+        $this->sinif;
+    }
+	function setIlce($ilce)
+	{  
+		$this -> ilce = $ilce; 
+	}
+	function getIlce() 
+	{
+        $this->ilce;
+    }
+	function setIl($il)
+	{  
+		$this -> il = $il; 
+	}
+	function getIl() 
+	{
+        $this->il;
     }
 	
 	
