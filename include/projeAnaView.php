0378eb6484c07715e3b1da6fa3f442b86f447bea
mustafazorbaz <mustafazorbaz@hotmail.com>
2016-03-27 20:48:11 +0300

    Öğrenci Proje View -> Test,Class ve Controller
    Classın test için kullandık

---
+++ include/projeAnaView.php
@@ -0,0 +1,24 @@
+﻿
+
+<html>
+<head>
+<title>
+Ogrenci Projeleri
+</title>
+<body>
+<div class="content" id="content">
+
+  
+  <p class="ac"><a href="projeKaydetView.php">Kayıt Ekle</a></p>
+  <form name="form1" method="post" action="projeAnaView.php">
+    <input type="submit" name="listele" id="listele" value="LISTELE">
+</form>
+  <p>&nbsp;</p>
+   <?php
+
+include_once("projeControler.php");
+?>
+
+</div>
+</body>
+</html>
