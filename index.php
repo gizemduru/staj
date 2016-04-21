fd7409947f528a159926b1d9eb8e7b91a791106a
Sefa Durmaz <yzmsefa@gmail.com>
2016-04-21 13:34:36 +0300

    Revert "Revert "index footer ve header tasarım""
    This reverts commit 2205d7c90c02dad710e40a0e838f683791e14d49.

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title></title>
+<link href="css/index.css" rel="stylesheet" type="text/css" />
+<link href="css/mainFooter.css" rel="stylesheet" type="text/css" />
+<link href="css/mainHeader.css" rel="stylesheet" type="text/css" />
+<meta charset="utf-8"/>
+</head>
+<body>
+	<div id="header">
+	<?php include_once("/include/mainHeader.php");?>
+	</div>
+	<div id="main">
+		
+	</div>
+	<div id="footer">
+		<?php include_once("/include/mainFooter.php");?>
+	</div>
+</body>
+</html>
