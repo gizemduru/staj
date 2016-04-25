04d9af536e1b79876fae7e4ed0f0f9ebae90fa3c
Burak FIRAT <burakfiratz@gmail.com>
2016-04-25 14:55:19 +0300

    scs
    daw

--- include/slider.php
+++ include/slider.php
@@ -1,14 +1,14 @@
 ﻿<div class="ana" >
 	<div class="gosteri">
 		<ul>
-			<li><img src="../img/1.jpg" title="awd" /></li>
-			<li><img src="../img/2.jpg" title="jgyjgy" /></li>
-			<li><img src="../img/3.jpg" /></li>
-			<li><img src="../img/4.jpg" /></li>
-			<li><img src="../img/5.jpg" /></li>
-			<li><img src="../img/6.jpg" /></li>
-			<li><img src="../img/7.jpg" /></li>
-			<li><img src="../img/8.jpg" /></li>
+			<li><img src="img/1.jpg" title="awd" /></li>
+			<li><img src="img/2.jpg" title="jgyjgy" /></li>
+			<li><img src="img/3.jpg" /></li>
+			<li><img src="img/4.jpg" /></li>
+			<li><img src="img/5.jpg" /></li>
+			<li><img src="img/6.jpg" /></li>
+			<li><img src="img/7.jpg" /></li>
+			<li><img src="img/8.jpg" /></li>
 			<li id="bitti"><img src="" /></li>
 		</ul>
 	</div>
