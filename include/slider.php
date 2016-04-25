a40b18aae5f6780f57bdadb71173d46c4e72bd83
Burak FIRAT <burakfiratz@gmail.com>
2016-04-25 15:20:53 +0300

    sa
    dawda

--- include/slider.php
+++ include/slider.php
@@ -1,15 +1,15 @@
 ﻿<div class="ana" >
 	<div class="gosteri">
 		<ul>
-			<li><img src="img/1.jpg" title="awd" /></li>
-			<li><img src="img/2.jpg" title="jgyjgy" /></li>
-			<li><img src="img/3.jpg" /></li>
-			<li><img src="img/4.jpg" /></li>
-			<li><img src="img/5.jpg" /></li>
-			<li><img src="img/6.jpg" /></li>
-			<li><img src="img/7.jpg" /></li>
-			<li><img src="img/8.jpg" /></li>
-			<li id="bitti"><img src="" /></li>
+			<li><img src="img/1.jpg" title="awd" class="slimg" /></li>
+			<li><img src="img/2.jpg" title="jgyjgy" class="slimg" /></li>
+			<li><img src="img/3.jpg" class="slimg"  /></li>
+			<li><img src="img/4.jpg" class="slimg"  /></li>
+			<li><img src="img/5.jpg" class="slimg"  /></li>
+			<li><img src="img/6.jpg" class="slimg"  /></li>
+			<li><img src="img/7.jpg" class="slimg"  /></li>
+			<li><img src="img/8.jpg" class="slimg"  /></li>
+			<li id="bitti"><img src="" class="slimg" /></li>
 		</ul>
 	</div>
 	<div class="secici">1/8</div>
